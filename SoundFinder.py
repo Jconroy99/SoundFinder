b33fc3046e69a66b9706cba26bb62226063f87f4
Jackson Conroy <Jacksonconroy99@gmail.com>
2022-10-16 15:52:57 -0400

    Program works and returns a list of similar songs; still takes 20 minutes because of requests to Spotify; Filter values need to be tweaked and code needs to broken up into functions

--- SoundFinder.py
+++ SoundFinder.py
@@ -1,3 +1,5 @@
+from dis import Instruction
+from types import TracebackType
 import pandas as pd 
 import sqlite3
 from pathlib import Path
@@ -29,20 +31,28 @@ sql_query_delete = 'DELETE FROM spotify_table WHERE region!="United States"'
 c.execute(sql_query_delete)
 conn.commit()
 
-#SQL query for every URL and storing it in list urls and close connection
+#SQL query for every URL and storing it in list urls_slices and close connection
 sql_select = 'Select url FROM spotify_table'
-urls = c.execute(sql_select).fetchall()
-conn.close()
 
-#Iterate through each song url and get audio features
-urls_len = len(urls)
-track_features = []
-for i in range(100):
-    #Returns list of dictionaries of a song's features/qualities
-    track_features.append(sp.audio_features(urls[i]))
-    print(track_features[i])
+#This returns list of tuples e.g., [('url1',) (url2,)]
+song_rows = c.execute(sql_select).fetchall()
 
-#Define global variables for inputted song's features for comparison
+#List comprehension to flatten rows into list of urls i.e., [url1, url2,...]
+urls_list = [row[0] for row in song_rows]
+
+#List comprehension to return a
+#list of batches of 100 urls (or slices) that is passed to audio_features()
+urls_list_len = len(urls_list)
+urls_slices = [urls_list[i:i+100] for i in range(0,urls_list_len,100)]
+
+
+#Iterate through each batch of 100 song urls and get audio features
+urls_len = len(urls_slices)
+
+#List of dictionaries of audio features 
+track_features = [track_feature for urls_slice in urls_slices for track_feature in sp.audio_features(urls_slice)]
+
+#Define global variables for inputted song's features for comparison (will eventually take as input from user)
 song_link = 'https://open.spotify.com/track/02shCNmb6IvgB5jLqKjtkK?si=a35c6a01bc91447d'
 song_input = sp.audio_features(song_link)
 
@@ -59,19 +69,52 @@ tempo = song_input[0]['tempo']
 
 #Comparing acousticness loop:
 acous_list = []
-for i in range(100):
-    if track_features[i][0]['acousticness'] >= (acousticness - 0.1) and track_features[i][0]['acousticness'] <= (acousticness + 0.1):
-        acous_list.append(track_features[i])
+for track in track_features:
+    if track['acousticness'] >= (acousticness - 0.1) and track['acousticness'] <= (acousticness + 0.1):
+        acous_list.append(track)
+print(len(acous_list))
 
-acous_len = len(acous_list)
+#Comparing danceability:
+dance_list = []
+for track in acous_list:
+    if track['danceability'] >= (danceability - 0.1) and track['danceability'] <= (danceability + 0.1):
+        dance_list.append(track)
+print(len(dance_list))
 
-# dance_list = []
-# for i in range(acous_len):
-#     if acous_list[i]['danceability'] >= (danceability - 0.1) and acous_list[i]['danceability'] <= (danceability + 0.1):
-#         dance_list.append(acous_list[i])
-#     else:
-#         print(acous_list[i]) 
+#Comparing energy:
+energy_list = []
+for track in dance_list:
+    if track['energy'] >= (energy - 0.1) and track['energy'] <= (energy + 0.1):
+        energy_list.append(track)
+print(len(energy_list))
 
-# dance_len = len(dance_list)
-# print(dance_len)
+# #Comparing valence:
+# valence_list = []
+# for track in energy_list:
+#     if track['valence'] >= (valence - 0.1) and track['valence'] <= (valence + 0.1):
+#         valence_list.append(track)
+# print(len(valence_list))
+
+#Comparing instrumentalness:
+instrumetalness_list = []
+for track in energy_list:
+    if track['instrumentalness'] >= (instrumetalness - 0.3) and track['instrumentalness'] <= (instrumetalness + 0.3):
+        instrumetalness_list.append(track)
+print(len(instrumetalness_list))
+
+#Comparing speechiness:
+speechiness_list = []
+for track in instrumetalness_list:
+    if track['speechiness'] >= (speechiness - 0.1) and track['speechiness'] <= (speechiness + 0.1):
+        speechiness_list.append(track)
+print(len(speechiness_list))
+
+#Comparing tempo:
+tempo_list = []
+for track in speechiness_list:
+    if track['tempo'] >= (tempo - 10) and track['tempo'] <= (tempo + 10):
+        tempo_list.append(track)
+print(len(tempo_list))
+print(tempo_list)
+
 
